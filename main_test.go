5579290c42e31bb6cdcd892f0e0ab39b8947a117
Amit Kumar <94365328+aredhu88@users.noreply.github.com>
2023-10-26 19:36:20 +0530

    Update main_test.go
    Signed-off-by: Amit Kumar <94365328+aredhu88@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -17,25 +17,15 @@ func TestRunsSuite(t *testing.T) {
 	// The manifest path should contain a file named config.json that is a
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
-	//
 
-	// Uncomment the below fixture when implementing your custom DNS provider
-	//fixture := dns.NewFixture(&customDNSProviderSolver{},
-	//	dns.SetResolvedZone(zone),
-	//	dns.SetAllowAmbientCredentials(false),
-	//	dns.SetManifestPath("testdata/my-custom-solver"),
-	//	dns.SetBinariesPath("_test/kubebuilder/bin"),
-	//)
-	solver := example.New("59351")
-	fixture := dns.NewFixture(solver,
-		dns.SetResolvedZone("example.com."),
-		dns.SetManifestPath("testdata/my-custom-solver"),
-		dns.SetDNSServer("127.0.0.1:59351"),
-		dns.SetUseAuthoritative(false),
+  //Options from https://github.com/jetstack/cert-manager/blob/master/test/acme/dns/options.go
+	fixture := dns.NewFixture(&DNSMadeEasyProviderSolver{},
+		dns.SetResolvedZone(zone),
+		dns.SetAllowAmbientCredentials(false),
+		dns.SetManifestPath("testdata/dnsmadeeasy"),
+		dns.SetBinariesPath("_out/kubebuilder/bin"),
+    //dns.SetDNSServer("ns1.sandbox.dnsmadeeasy.com:53"),
 	)
-	//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
-	//fixture.RunConformance(t)
-	fixture.RunBasic(t)
-	fixture.RunExtended(t)
 
+	fixture.RunConformance(t)
 }
